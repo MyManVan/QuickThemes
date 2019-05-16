85412ad6731f7c07e997db7d549b111e618726e2
MyManVan <kcjackkelly@gmail.com>
2019-05-16 14:22:25 -0500

    Updated youtube browse theme, added styled console log.

--- quickthemes.js
+++ quickthemes.js
@@ -1,5 +1,3 @@
-
-
 function addCss(styleSheet) {
   const head = document.head;
   const link = document.createElement("link");
@@ -10,15 +8,32 @@ function addCss(styleSheet) {
 
   head.appendChild(link);
 }
-
+/*THEMES*/
 const retrowaveYoutube = chrome.extension.getURL("themes/retrowave/youtube.css");
 const retrowaveSpotify = chrome.extension.getURL("themes/retrowave/spotify.css");
+const logStyles = [
+    'background: linear-gradient(#FFA1F1, #258EA6)'
+    , 'border: 1px solid #3E0E02'
+    , 'color: white'
+    , 'display: block'
+    , 'text-shadow: 0 1px 0 rgba(0, 0, 0, 0.3)'
+    , 'box-shadow: 0 1px 0 rgba(255, 255, 255, 0.4) inset, 0 5px 3px -5px rgba(0, 0, 0, 0.5), 0 -13px 5px -10px rgba(255, 255, 255, 0.4) inset'
+    , 'line-height: 40px'
+    , 'text-align: center'
+    , 'font-weight: bold'
+    , 'padding: 25px'
+].join(';');
 
+
+
+/*ONLOAD*/
 window.addEventListener('load', function() {
   console.log("Loaded QuickThemes" + document.URL);
   if (document.URL == "https://www.youtube.com/") {
-    addCss(retrowaveYoutube);
+      addCss(retrowaveYoutube);
+      console.log('%c QuickThemes Loaded. Enjoy!', logStyles);
   } else if (document.URL == "https://open.spotify.com/" || "https://play.spotify.com") {
-    addCss(retrowaveSpotify);
+      addCss(retrowaveSpotify);
+      console.log('%c QuickThemes Loaded. Enjoy!', logStyles);
   }
 });
