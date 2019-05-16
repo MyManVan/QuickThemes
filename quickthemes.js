a42427578f9b4c3839c22087bef9d3de2c858fd8
MyManVan <kcjackkelly@gmail.com>
2019-05-16 11:54:00 -0500

    Added youtube theme, spotify theme. Added popup

--- quickthemes.js
+++ quickthemes.js
@@ -1,6 +1,4 @@
-window.addEventListener('load', function() {
-  console.log("Loaded QuickThemes");
-});
+
 
 function addCss(styleSheet) {
   const head = document.head;
@@ -13,5 +11,14 @@ function addCss(styleSheet) {
   head.appendChild(link);
 }
 
-const retrowave = chrome.extension.getURL("themes/retrowave/retrowave.css");
-addCss(retrowave);
+const retrowaveYoutube = chrome.extension.getURL("themes/retrowave/youtube.css");
+const retrowaveSpotify = chrome.extension.getURL("themes/retrowave/spotify.css");
+
+window.addEventListener('load', function() {
+  console.log("Loaded QuickThemes" + document.URL);
+  if (document.URL == "https://www.youtube.com/") {
+    addCss(retrowaveYoutube);
+  } else if (document.URL == "https://open.spotify.com/" || "https://play.spotify.com") {
+    addCss(retrowaveSpotify);
+  }
+});
